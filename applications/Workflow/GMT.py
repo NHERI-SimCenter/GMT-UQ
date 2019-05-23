b56f9a8b6d2c08a024f7de9b01b79e03014730bd
fmckenna <fmckenna@berkeley.edu>
2019-05-22 17:15:21 -0700

    fmk - renaming some files

--- applications/Workflow/GMT.py
+++ applications/Workflow/GMT.py
@@ -146,82 +146,6 @@ def main(run_type, inputFile, applicationsRegistry):
         else:
             raise WorkFlowInputError('Need an Events Entry in Applications')
 
-        #
-        # get modeling application and its data
-        #
-
-        if 'Modeling' in available_apps:
-            modelingApp = available_apps['Modeling']
-
-            if 'Application' in modelingApp:
-                modelingApplication = modelingApp['Application']
-
-                # check modeling app in registry, if so get full executable path
-                modelingAppData = modelingApp['ApplicationData']
-                if modelingApplication in Applications['ModelingApplications'].keys():
-                    modelingAppExe = Applications['ModelingApplications'].get(modelingApplication)
-                    modelingAppExeLocal = posixpath.join(localAppDir,modelingAppExe)
-                    modelingAppExeRemote = posixpath.join(remoteAppDir,modelingAppExe)
-                else:
-                    raise WorkFlowInputError('Modeling application %s not in registry' % modelingApplication)
-
-            else:
-                raise WorkFlowInputError('Need a ModelingApplication in Modeling data')
-
-
-        else:
-            raise WorkFlowInputError('Need a Modeling Entry in Applications')
-
-        #
-        # get edp application and its data .. CURRENTLY MODELING APP MUST CREATE EDP
-        #
-
-        if 'EDP' in available_apps:
-            edpApp = available_apps['EDP']
-            
-            if 'Application' in edpApp:
-                edpApplication = edpApp['Application']
-                
-                # check modeling app in registry, if so get full executable path
-                edpAppData = edpApp['ApplicationData']
-                if edpApplication in Applications['EDPApplications'].keys():
-                    edpAppExe = Applications['EDPApplications'].get(edpApplication)
-                    edpAppExeLocal = posixpath.join(localAppDir,edpAppExe)
-                    edpAppExeRemote = posixpath.join(remoteAppDir,edpAppExe)
-                else:
-                    raise WorkFlowInputError('EDP application {} not in registry'.format(edpApplication))
-                
-            else:
-                raise WorkFlowInputError('Need an EDPApplication in EDP data')
-            
-        else:
-            raise WorkFlowInputError('Need an EDP Entry in Applications')
-
-        #
-        # get simulation application and its data 
-        #
-
-        if 'Simulation' in available_apps:
-            simulationApp = available_apps['Simulation']
-
-            if 'Application' in simulationApp:
-                simulationApplication = simulationApp['Application']
-
-                # check modeling app in registry, if so get full executable path
-                simAppData = simulationApp['ApplicationData']
-                if simulationApplication in Applications['SimulationApplications'].keys():
-                    simAppExe = Applications['SimulationApplications'].get(simulationApplication)
-                    simAppExeLocal = posixpath.join(localAppDir,simAppExe)
-                    simAppExeRemote = posixpath.join(remoteAppDir,simAppExe)
-                else:
-                    raise WorkFlowInputError('Simulation application {} not in registry'.format(simulationApplication))
-
-            else:
-                raise WorkFlowInputError('Need an SimulationApplication in Simulation data')
-
-
-        else:
-            raise WorkFlowInputError('Need a Simulation Entry in Applications')
 
         if 'UQ' in available_apps:
             uqApp = available_apps['UQ']
@@ -260,18 +184,15 @@ def main(run_type, inputFile, applicationsRegistry):
         #  - perform Simulation
         #  - getDL
 
-        bimFILE = 'dakota.json'
+        inputFILE = 'dakota.json'
         eventFILE = 'EVENT.json'
-        samFILE = 'SAM.json'
-        edpFILE = 'EDP.json'
-        simFILE = 'SIM.json'
         driverFile = 'driver'
 
         # open driver file & write building app (minus the --getRV) to it
         driverFILE = open(driverFile, 'w')
 
         # get RV for event
-        eventAppDataList = ['"{}"'.format(eventAppExeRemote), '--filenameBIM', bimFILE, '--filenameEVENT', eventFILE]
+        eventAppDataList = ['"{}"'.format(eventAppExeRemote), '--filenameBIM', inputFILE, '--filenameEVENT', eventFILE]
         if (eventAppExe.endswith('.py')):
             eventAppDataList.insert(0, 'python')
 
@@ -293,86 +214,12 @@ def main(run_type, inputFile, applicationsRegistry):
         command, result, returncode = runApplication(eventAppDataList)
         log_output.append([command, result, returncode])
 
-        # get RV for building model
-        modelAppDataList = ['"{}"'.format(modelingAppExeRemote), '--filenameBIM', bimFILE, '--filenameEVENT', eventFILE, '--filenameSAM',
-                            samFILE]
-
-        if (modelingAppExe.endswith('.py')):
-            modelAppDataList.insert(0, 'python')
-
-        for key in modelingAppData.keys():
-            modelAppDataList.append(u'--' + key)
-            modelAppDataList.append(u'' + modelingAppData.get(key))
-
-        for item in modelAppDataList:
-            driverFILE.write('%s ' % item)
-        driverFILE.write('\n')
-
-        modelAppDataList.append('--getRV')
-
-        if (modelingAppExe.endswith('.py')):
-            modelAppDataList[1] = modelingAppExeLocal
-        else:
-            modelAppDataList[0] = modelingAppExeLocal
-
-        command, result, returncode = runApplication(modelAppDataList)
-        log_output.append([command, result, returncode])
-
-
-        # get RV for EDP!
-        edpAppDataList = ['"{}"'.format(edpAppExeRemote), '--filenameBIM', bimFILE, '--filenameEVENT', eventFILE, '--filenameSAM', samFILE,
-                          '--filenameEDP', edpFILE]
-
-        if (edpAppExe.endswith('.py')):
-            edpAppDataList.insert(0, 'python')
-
-        for key in edpAppData.keys():
-            edpAppDataList.append(u'--' + key)
-            edpAppDataList.append(u'' + edpAppData.get(key))
-
-        for item in edpAppDataList:
-            driverFILE.write('%s ' % item)
-        driverFILE.write('\n')
-
-        if (edpAppExe.endswith('.py')):
-            edpAppDataList[1] = edpAppExeLocal
-        else:
-            edpAppDataList[0] = edpAppExeLocal
-
-        edpAppDataList.append('--getRV')
-        command, result, returncode = runApplication(edpAppDataList)
-        log_output.append([command, result, returncode])
-
-        # get RV for Simulation
-        simAppDataList = ['"{}"'.format(simAppExeRemote), '--filenameBIM', bimFILE, '--filenameSAM', samFILE, '--filenameEVENT', eventFILE,
-                          '--filenameEDP', edpFILE, '--filenameSIM', simFILE]
-
-        if (simAppExe.endswith('.py')):
-            simAppDataList.insert(0, 'python')
-
-        for key in simAppData.keys():
-            simAppDataList.append(u'--' + key)
-            simAppDataList.append(u'' + simAppData.get(key))
-
-        for item in simAppDataList:
-            driverFILE.write('%s ' % item)
-        driverFILE.write('\n')
-
-        simAppDataList.append('--getRV')
-        if (simAppExe.endswith('.py')):
-            simAppDataList[1] = simAppExeLocal
-        else:
-            simAppDataList[0] = simAppExeLocal
-
-        command, result, returncode = runApplication(simAppDataList)
-        log_output.append([command, result, returncode])
-
 
         # perform the simulation
         driverFILE.close()
 
-        uqAppDataList = ['"{}"'.format(uqAppExeLocal), '--filenameBIM', bimFILE, '--filenameSAM', samFILE, '--filenameEVENT', eventFILE,
-                         '--filenameEDP', edpFILE, '--filenameSIM', simFILE, '--driverFile', driverFile]
+        uqAppDataList = ['"{}"'.format(uqAppExeLocal), '--filenameBIM', inputFILE, '--filenameEVENT', eventFILE,
+                         '--driverFile', driverFile]
 
         if (uqAppExe.endswith('.py')):
             uqAppDataList.insert(0, 'python')
